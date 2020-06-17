95647febf118826dc871377c36863bf5f5847b27
csuescun <csuescun@campus.udes.edu.co>
2020-06-16 21:32:46 -0500

    paginate empresa

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
     $router->put('/empresa/{id}', 'EmpresaController@put');
     $router->patch('/empresa/{id}', 'EmpresaController@patch');
     $router->delete('/empresa/{id}', 'EmpresaController@delete');
+    $router->get('/paginated/empresa', 'EmpresaController@paginate');
 
     // TIPOPRODUCTO
     $router->get('/tipoproducto', 'TipoProductoController@get');
