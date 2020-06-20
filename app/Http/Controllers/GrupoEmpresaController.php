2e4294fc0c0ac9c06fad623799a8038125cba007
Jairo <snakesands@gmail.com>
2020-06-20 00:06:09 -0500

    mejora grupoempresa

--- app/Http/Controllers/GrupoEmpresaController.php
+++ app/Http/Controllers/GrupoEmpresaController.php
@@ -32,6 +32,77 @@ class GrupoEmpresaController extends Controller
         }
         return response()->json(['data' => $res]);
     }
+
+    public function paginate(Request $request){
+        $per_page = 20;
+        $current_page = 1;
+        $params = $request->request->all();
+        if(array_key_exists('per_page', $params)){
+            $per_page = intval($params['per_page']);
+        }
+        if(array_key_exists('current_page', $params)){
+            $current_page = intval($params['current_page']);
+        }
+        $where = [];
+        $pagination = new \stdClass;
+        $pagination->pagination = new \stdClass;
+
+        if(array_key_exists('grupo', $params)){
+            $where['grupo.id'] = $params['grupo']; 
+            $pagination->pagination->grupo = intval($params['grupo']);
+        }
+        if(array_key_exists('empresa', $params)){
+            $where['empresa.id'] = $params['empresa']; 
+            $pagination->pagination->empresa = intval($params['empresa']);
+        }    
+        if(array_key_exists('search', $params)){
+            if($params['search'] != ''){
+                $where[] = ['empresa.nombre', 'like', '%'.$params['search'].'%']; 
+                $pagination->pagination->search = $params['search'];
+            }
+        }    
+
+        $total = GrupoEmpresa::select('grupoempresa.id')
+        ->join('empresa', 'empresa.id', '=', 'grupoempresa.empresa_id')
+        ->join('grupo', 'grupo.id', '=', 'grupoempresa.grupo_id')
+        ->where($where)
+        ->distinct()
+        ->get();
+        
+        $total = count($total);
+        $pagination->pagination->last_page =  ceil($total/$per_page);
+
+        if($current_page>$pagination->pagination->last_page){
+            $current_page = $pagination->pagination->last_page;
+        }
+        elseif($current_page<1){
+            $current_page = 1;
+        }
+        $skip = $per_page * ($current_page-1);
+
+        $data = GrupoEmpresa::select('grupoempresa.*')
+            ->join('empresa', 'empresa.id', '=', 'grupoempresa.empresa_id')
+            ->join('grupo', 'grupo.id', '=', 'grupoempresa.grupo_id')
+            ->where($where)
+            ->with('empresa')
+            ->skip($skip)
+            ->take($per_page)
+            ->distinct()
+            ->get();
+        
+        $pagination->pagination->current_page =  $current_page;
+        $pagination->pagination->per_page =  $per_page;
+        $pagination->pagination->total =  $total;
+        $from = $skip + 1;
+        $pagination->pagination->from =  $from;
+        $to = ($total < $from + $per_page) ? $total : $per_page * $current_page;
+        $pagination->pagination->to =  $to;
+        $pagination->pagination->showing =  $to - $from + 1;
+        $pagination->data =  $data;
+        
+        return response()->json(['data' => $pagination]);
+    }
+
     
     public function find($id){
         $model = GrupoEmpresa::find($id);
