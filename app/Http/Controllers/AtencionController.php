2c2304359d14a1615e6a49c225dea47e7757867c
Jairo <snakesands@gmail.com>
2020-07-14 21:54:23 -0500

    mejora atencion

--- app/Http/Controllers/AtencionController.php
+++ app/Http/Controllers/AtencionController.php
@@ -19,7 +19,7 @@ class AtencionController extends Controller
      ];
 
     protected $rules = [
-        'empresa_id' => 'required|unique:atencion', 'estado' => 'required'
+        'empresa_id' => 'required', 'estado' => 'required'
     ];
     
     public function all(){
@@ -39,6 +39,73 @@ class AtencionController extends Controller
         }
         return response()->json(['data' => $res]);
     }
+
+    //paginacion
+    public function paginate(Request $request){
+        $per_page = 20;
+        $current_page = 1;
+        $params = $request->request->all();
+        if(array_key_exists('per_page', $params)){
+            $per_page = intval($params['per_page']);
+        }
+        if(array_key_exists('current_page', $params)){
+            $current_page = intval($params['current_page']);
+        }
+        $where = [];
+        $pagination = new \stdClass;
+        $pagination->pagination = new \stdClass;
+
+        if(array_key_exists('empresa', $params)){
+            $where['empresa.id'] = $params['empresa']; 
+            $pagination->pagination->empresa = intval($params['empresa']);
+        }    
+        if(array_key_exists('search', $params)){
+            if($params['search'] != ''){
+                $where[] = ['empresa.nombre', 'like', '%'.$params['search'].'%']; 
+                $pagination->pagination->search = $params['search'];
+            }
+        }    
+        //count de registros
+        $total = Atencion::select('atencion.id')
+        ->join('empresa', 'empresa.id', '=', 'atencion.empresa_id')
+        ->where($where)
+        ->distinct()
+        ->get();
+        
+        $total = count($total);
+        $pagination->pagination->last_page =  ceil($total/$per_page);
+
+        if($current_page>$pagination->pagination->last_page){
+            $current_page = $pagination->pagination->last_page;
+        }
+        elseif($current_page<1){
+            $current_page = 1;
+        }
+        $skip = $per_page * ($current_page-1);
+        //lista
+        $data = Oferta::select('atencion.*')
+            ->join('empresa', 'empresa.id', '=', 'atencion.empresa_id')
+            ->where($where)
+            ->with('empresa')
+            
+            ->skip($skip)
+            ->take($per_page)
+            ->distinct()
+            ->get();
+        
+        $pagination->pagination->current_page =  $current_page;
+        $pagination->pagination->per_page =  $per_page;
+        $pagination->pagination->total =  $total;
+        $from = $skip + 1;
+        $pagination->pagination->from =  $from;
+        $to = ($total < $from + $per_page) ? $total : $per_page * $current_page;
+        $pagination->pagination->to =  $to;
+        $pagination->pagination->showing =  $to - $from + 1;
+        $pagination->data =  $data;
+        
+        return response()->json(['data' => $pagination]);
+    }
+
     
     public function find($id){
         $model = Atencion::find($id);
@@ -56,14 +123,14 @@ class AtencionController extends Controller
     
     public function put(Request $request, $id){
         $rules = $this->rules;
-        $rules['empresa_id'] .= ',empresa_id,'.$id;
+        
         $fields = $this->fields;
         return $this->save($request, Atencion::find($id), $rules, $fields);
     }
     
     public function patch(Request $request, $id){
         $rules = $this->rules;
-        $rules['empresa_id'] .= ',empresa_id,'.$id;
+        
         $fields = $this->fields;
         foreach ($rules as $key => $value) {
             if(!$request->has($key)){
